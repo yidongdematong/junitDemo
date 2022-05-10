2aea04a18f1b32901464d2cd4f91a8312f7dee3a
wangh <yidongdematong@163.com>
2022-05-10 20:07:45 +0800

    @DisplayName

---
+++ src/main/java/com/wong/main/Portal.java
@@ -0,0 +1,15 @@
+package com.wong.main;
+
+/**
+ * @author ：bobby
+ * @date ：Created in 2022/5/10 16:34
+ * @description：入口
+ * @modified By：
+ * @version: 0.1
+ */
+public class Portal {
+
+    void FirstFunction (){
+        System.out.println("第一个测试方法");
+    }
+}
---
+++ src/test/java/com/wong/main/PortalTest.java
@@ -0,0 +1,15 @@
+package com.wong.main;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("我是第一个测试类的名字")
+class PortalTest {
+
+    @DisplayName("我是第一个测试方法的名字")
+    @Test
+    void firstFunction() {
+    }
+}
